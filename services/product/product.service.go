5a24b4d81983194a6285e59640283177856631a7
Badar Wildani <wildanie12@gmail.com>
2022-08-07 21:47:17 +0700

    add: product service implementation

---
+++ services/product/product.service.go
@@ -0,0 +1,48 @@
+package product
+
+import _models "github.com/wildanie12/go-hex-arch-sample/models"
+
+// Service struct
+type Service struct {
+
+}
+
+// New Service instance
+func New() *Service {
+	return &Service{}
+}
+
+var products = []_models.Product{
+	{
+		ID: 1,
+		Name: "Mouse keren",
+		Description: "Mouse dengan presisi 6000 DPI yang bisa dipake buat mouse",
+		Price: 30000,
+		Category: "Aksesoris Komputer",
+		Seller: "Wawan",
+		Slug: "mouse-keren",
+	},
+	{
+		ID: 2,
+		Name: "Keyboard keren",
+		Description: "keyboard disertai dengan pencetan untuk mengetik cocok sekali untuk tukang ketik",
+		Price: 150000,
+		Category: "Aksesoris Komputer",
+		Seller: "Wawan",
+		Slug: "keyboard-keren",
+	},
+	{
+		ID: 3,
+		Name: "Monitor keren",
+		Description: "Monitor yang dapat menampilkan gambar untuk dilihat",
+		Price: 1800000,
+		Category: "Aksesoris Komputer",
+		Seller: "Wawan",
+		Slug: "monitor-keren",
+	},
+}
+
+// List of products
+func (s Service) List() ([]_models.Product, error) {
+	return products, nil
+}
