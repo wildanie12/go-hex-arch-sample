6b494d43fb0eb97f0cc4c21e886920e8aa74291e
Badar Wildani <wildanie12@gmail.com>
2022-08-07 20:11:46 +0700

    add: json encode utility

---
+++ utils/json/json.go
@@ -0,0 +1,17 @@
+package json
+
+import (
+	"encoding/json"
+	"log"
+
+	"github.com/wildanie12/go-hex-arch-sample/utils/color"
+)
+
+// Encode your object, useful for debugging.
+func Encode(obj interface{}) string {
+	json, err := json.MarshalIndent(obj, "", "  ")
+	if err != nil {
+		log.Println(color.ThisF("yellow", "[JsonUtil] fail to encode json: %v", err))
+	}
+	return string(json)
+}
