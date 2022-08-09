87c45cd80f6abb8f747a316a9bc7a8a9fb793c70
Badar Wildani <wildanie12@gmail.com>
2022-08-10 00:12:46 +0700

    add: new product repository interface

---
+++ repositories/product/product.interface.go
@@ -0,0 +1,17 @@
+package repositories
+
+import _entities "github.com/wildanie12/go-hex-arch-sample/entities"
+
+type ProductInterface interface {
+	
+	FindAll() ([]_entities.Product, error)
+
+	Find(id int) (_entities.Product, error)
+
+	Store(product _entities.Product) (_entities.Product, error)
+
+	Update(product _entities.Product) (_entities.Product, error)
+
+	Delete(id int) error
+
+}
