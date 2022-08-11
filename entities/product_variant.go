1745a1eea9d2114e2107653ce5b2c723575685a2
Badar Wildani <wildanie12@gmail.com>
2022-08-11 12:19:19 +0100

    add: product variant entity

---
+++ entities/product_variant.go
@@ -0,0 +1,11 @@
+package entities
+
+type ProductVariant struct {
+	ID uint 			`gorm:"primaryKey"`
+	ProductID uint
+	Sku string			`gorm:"size:255" faker:"uuid_hyphenated"`
+	Name string			`gorm:"size:255" faker:"word"`
+	Price int64
+	Description string	`faker:"paragraph"`
+	Product Product		`gorm:"foreignKey:ProductID;references:ID"`
+}
