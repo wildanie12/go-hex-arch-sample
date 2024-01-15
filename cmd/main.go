b54381a6fdb18d4078e57fc079ca3f49b9663ddd
Badar Wildani <wildanie12@gmail.com>
2024-01-15 10:10:26 +0700

    feat: app dependency injections

---
+++ cmd/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/wildanie12/go-hex-arch-sample/internal"
+	appHttp "github.com/wildanie12/go-hex-arch-sample/internal/http"
+	cartSrv "github.com/wildanie12/go-hex-arch-sample/internal/service/cart"
+	productSrv "github.com/wildanie12/go-hex-arch-sample/internal/service/product"
+)
+
+func main() {
+	// -- construct your services here...
+	s := internal.ServiceAPI{
+		CartService:    cartSrv.New(),
+		ProductService: productSrv.New(),
+	}
+
+	// -- construct your presenters here...
+	appHttp := appHttp.New()
+
+	// -- assign presenters
+	frontendPresenter := internal.NewPresenter(appHttp, &s)
+
+	// execute presenters
+	forever := make(chan bool)
+	fmt.Println("running enabled presenter...")
+	frontendPresenter.Start()
+
+	fmt.Println("App server is started...")
+	<-forever
+}
