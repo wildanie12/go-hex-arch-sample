7510c095d72f3e58a293a67f8bbbfaad7f4e2145
Badar Wildani <wildanie12@gmail.com>
2024-01-15 10:10:26 +0700

    refactor: delete existing

--- app/grpc/route.go
+++
@@ -1,9 +0,0 @@
-package grpc
-
-import (
-	_product "github.com/wildanie12/go-hex-arch-sample/app/grpc/models/product"
-)
-
-func (rpc *MainGRPC) defineRoute(provider gRPCProvider) {
-	_product.RegisterProductServer(rpc.server, provider.handlers.product)
-}
--- main.go
+++ main.go
@@ -1,31 +1,4 @@
 package main
 
-import (
-	"os"
-
-	_ "github.com/joho/godotenv/autoload"
-	_grpc "github.com/wildanie12/go-hex-arch-sample/app/grpc"
-	_config "github.com/wildanie12/go-hex-arch-sample/config"
-	_migrate "github.com/wildanie12/go-hex-arch-sample/database/mysql/migrations"
-)
-
 func main() {
-	cli()
-	config := _config.New()
-
-	appGRPC := _grpc.New(config.GRPC.Host, config.GRPC.Port)
-	appGRPC.Start()
-}
-
-
-func cli() {
-	args := os.Args
-	if len(args) <= 1 {
-		return
-	}
-
-	if args[1] == "-m" || args[1] == "--migrate" {
-		_migrate.Start()
-	}
-	os.Exit(0)
 }
